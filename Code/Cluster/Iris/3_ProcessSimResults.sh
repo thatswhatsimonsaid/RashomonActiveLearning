3897fef612755ac8d74d89498df7f8d04541f14b
simondn <simonnguyen1999@gmail.com>
2025-02-20 23:58:45 -0800

    Running Iris with optimized code

--- Code/Cluster/Iris/3_ProcessSimResults.sh
+++ Code/Cluster/Iris/3_ProcessSimResults.sh
@@ -1,25 +1,30 @@
-#!/bin/bash
-
 ### Current Directory Name ###
 CURRENT_DIR=$(basename "$PWD")
 echo "Processing results for dataset: $CURRENT_DIR"
+cd ~/RashomonActiveLearning
+
+### Extract PassiveLearning Results ###
+cd ~/RashomonActiveLearning
+python Code/utils/Auxiliary/ProcessSimulationResults.py \
+    --DataType "$CURRENT_DIR" \
+    --ModelType "RandomForestClassification" \
+    --Categories "PLA0.pkl"
 
 ### Extract Random Forests Results ###
 cd ~/RashomonActiveLearning
-# python Code/utils/Auxiliary/ProcessSimulationResults.py \
-#     --DataType "$CURRENT_DIR" \
-#     --ModelType "RandomForestClassification" \
-#     --Categories "RF0.0.pkl"
+python Code/utils/Auxiliary/ProcessSimulationResults.py \
+    --DataType "$CURRENT_DIR" \
+    --ModelType "RandomForestClassification" \
+    --Categories "RFA0.pkl"
 
 ### Extract Duplicate TREEFARMS Results ###
-
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
     --DataType "$CURRENT_DIR" \
     --ModelType "TreeFarms" \
-    --Categories "DA0.01.pkl"
+    --Categories "DA0.025.pkl"
 
 ### Extract Unique TREEFARMS Results ###
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
     --DataType "$CURRENT_DIR" \
     --ModelType "TreeFarms" \
-    --Categories "UA0.01.pkl"
+    --Categories "UA0.025.pkl"
